34b1cc4dcd65a5734298fc8d7d729657c037251d
Pranav Jois <162716050+PranavJois@users.noreply.github.com>
2026-07-11 11:35:27 +0530

    Update full_adder.v

--- Verilog Codes/full_adder.v
+++ Verilog Codes/full_adder.v
@@ -3,4 +3,4 @@ module fa(input a,b,cin, output sum,carry);
   ha ha1(.a(a),.b(b),.sum(s1),.cout(c1));
   ha ha2(.a(s1),.b(cin),.sum(sum),.cout(c2));
   assign carry = c1 | c2;
-endmodule;
+endmodule
