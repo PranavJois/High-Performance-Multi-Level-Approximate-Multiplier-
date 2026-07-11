629a12463d758a8045fd175a8ffe8aa096914dc9
Pranav Jois <162716050+PranavJois@users.noreply.github.com>
2026-07-11 11:33:14 +0530

    Fix module instantiation for half adder

--- Verilog Codes/full_adder.v
+++ Verilog Codes/full_adder.v
@@ -1,6 +1,6 @@
 module fa(input a,b,cin, output sum,carry);
   wire s1,c1,c2;
-  HA ha1(.a(a),.b(b),.sum(s1),.cout(c1));
-  HA ha2(.a(s1),.b(cin),.sum(sum),.cout(c2));
+  ha ha1(.a(a),.b(b),.sum(s1),.cout(c1));
+  ha ha2(.a(s1),.b(cin),.sum(sum),.cout(c2));
   assign carry = c1 | c2;
 endmodule;
